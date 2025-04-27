a15dc15d29bd0a9a7d3d0ce0b3432cdd1b23dcac
Boffez <132230317+Boffez@users.noreply.github.com>
2025-04-27 02:33:45 +0200

    Add webapp/main/urls.py 1745714025425

--- webapp/main/urls.py
+++ webapp/main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 app_name = 'main'
 
@@ -7,4 +8,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('cars/', views.cars, name='cars'),
     path('about/', views.about, name='about'),
+    path('register/', views.register, name='register'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='main:index'), name='logout'),
 ]
